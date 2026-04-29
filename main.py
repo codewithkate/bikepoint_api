8d5a2873c3294caadff5bf257088e80452a7bc2e
Kate Crawford <kaitlencrawford@gmail.com>
2026-04-29 16:22:34 +0100

    refactor code to save a file for every bikepoint. add json file with all bikepoints.

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 import requests
 import json
 from datetime import datetime
+import time
 
 # variables
-# id =  "BikePoints_888"
 url = f"https://api.tfl.gov.uk/BikePoint/"
-response = requests.get(url)
-data = response.json()
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+response = requests.get(url)
+data = response.json() 
 
 if response.status_code == 200:
-    filename = f"BikePoints_all_{timestamp}.json"
-    with open(filename, "w") as file:
-        json.dump(data, file)
-    
-    print(f"File {filename} was successfully created. Woohoo 🥳")
 
+    for item in data:
+        id = item["id"]
+        id_response = requests.get(url+id)
+        id_data = response.json()
+        
+        if id_response.status_code == 200:    
+            filename = f"./bikepoints/{id}_{timestamp}.json"
+            with open(filename, "w") as file:
+                json.dump(id_data, file)
+            
+            print(f"File {filename} was successfully created. Woohoo 🥳")
+        else:
+            try: 
+                print(f"Error: {id_response.status_code} {id_data.get("message","no message found")}")
+            except: 
+                print(id)
 else:
     print(f"Error: {response.status_code} {data.get("message","no message found")}")
