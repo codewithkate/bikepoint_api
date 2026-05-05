fbe9f9ac2af7fb943203d75844e544411ba234cd
Kate Crawford <kaitlencrawford@gmail.com>
2026-05-05 14:04:24 +0100

    setup logging. loop through data directory and upload them to s3.

--- load.py
+++ load.py
@@ -1,7 +1,25 @@
 # libraries
-from dotenv import load_dotenv
-import boto3
 import os
+import boto3
+import logging
+from datetime import datetime
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Configure logs
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+log_filename = f"{log_dir}/load_{timestamp}.log"
+
+logging.basicConfig(
+    filename=log_filename,
+    format='%(asctime)s - %(levelname)s - %(message)s',    # format as time, level, and message
+    level=logging.INFO                                      # lowest level you want to capture with logs
+)
+
+logger = logging.getLogger()
+logger.info('Logger initialized')
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -18,7 +36,10 @@ s3_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_KEY
 )
 
-data = "data/bikepoints_2026-05-05_10-29-27.json"
-filename = "bikepoints_2026-05-05_10-29-27.json"
+data_dir = Path('data')
+files = list(data_dir.glob('*.json'))
 
-s3_client.upload_file(data, bucket, filename)
+for file in files:
+    filename = os.path.basename(file)
+    s3_client.upload_file(file, bucket, filename)
+    logger.info(f"{file} uploaded to S3")
