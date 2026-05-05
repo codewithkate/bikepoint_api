ab12fc524f8c86658ea3c0e4123299351b980834
Kate Crawford <kaitlencrawford@gmail.com>
2026-05-05 10:07:22 +0100

    refactor code. remove id loop. make data dir. retry if >500 response.

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
 # libraries
 import requests
 import json
-from datetime import datetime
 import time
+import os
+import logging 
+from datetime import datetime
 
 # variables
-# url = f"https://api.tfl.gov.uk/BikePoint/"
+url = f"https://api.tfl.gov.uk/BikePoint/"
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-# response = requests.get(url)
-# data = response.json() 
+response = requests.get(url)
+data = response.json()
 
-# if response.status_code == 200:
+count = 0
+max_tries = 3
 
-#     for item in data:
-#         id = item["id"]
-#         id_response = requests.get(url+id)
-#         id_data = response.json()
-        
-#         if id_response.status_code == 200:    
-#             filename = f"./bikepoints/{id}_{timestamp}.json"
-#             with open(filename, "w") as file:
-#                 json.dump(id_data, file)
-            
-#             print(f"File {filename} was successfully created. Woohoo 🥳")
-#         else:
-#             try: 
-#                 print(f"Error: {id_response.status_code} {id_data.get("message","no message found")}")
-#             except: 
-#                 print(id)
-# else:
-#     print(f"Error: {response.status_code} {data.get("message","no message found")}")
+while count <= max_tries:
 
-with open("BikePoints_all_2026-04-29_15-02-41.json", "r") as file:
-    data = json.load(file)
+    if 200 <= response.status_code < 200:
 
-failed_items = []
-
-for item in data:
-    try:
-        id = item["id"]
-        filename = f"./bikepoints_from_all/{id}_{timestamp}.json"
-        
+        dir = 'data'
+        os.makedirs(dir, exist_ok=True)
+        filename = f"{dir}/bikepoints_{timestamp}.json"
         with open(filename, "w") as file:
-            json.dump(item, file)
-
+            json.dump(data, file)
+            
         print(f"File {filename} was successfully created. Woohoo 🥳")
-    except:
-        failed_items.append(item)
+        break
 
-print("Total Failed Items:", len(failed_items))
+    elif response >= 500:
+        # RETRY after 10 seconds for these status codes
+        time.sleep(10)
+        count+=1
+        print(f"Attempt #{count} to call {url}")
+
+    else:
+        print(f"Error: {response.status_code} {response.get("message","no message found")}")
+        break
